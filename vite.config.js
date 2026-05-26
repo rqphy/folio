75e21bab488b0195d9f8cbcab0577a560c973f7c
rqphy <raphael.ferreira@hetic.net>
2026-05-26 21:18:46 +0200

    refactor: update vite config to separate three.js into its own cached chunk

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import glsl from 'vite-plugin-glsl'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+import glsl from "vite-plugin-glsl"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins:
-    [
-    	react(),
-		glsl()
-    ],
+	plugins: [react(), glsl()],
+	build: {
+		rollupOptions: {
+			output: {
+				manualChunks: {
+					three: ["three", "@react-three/fiber", "@react-three/drei"],
+				},
+			},
+		},
+	},
 })
